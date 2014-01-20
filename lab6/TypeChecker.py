b959cc44ff675d62bbb712132f528b47b937e687
turu <piotr.turu.turek@gmail.com>
2014-01-20 11:46:25 +0100

    Added lab6 specifics

---
+++ lab6/TypeChecker.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+from collections import defaultdict
+
+ttype = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: None)))
+for op in ['+', '-', '*', '/', '%', '<', '>', '<<', '>>', '|', '&', '^', '<=', '>=', '==', '!=']:
+    ttype[op]['int']['int'] = 'int'
+
+for op in ['+', '-', '*', '/']:
+    ttype[op]['int']['float'] = 'float'
+    ttype[op]['float']['int'] = 'float'
+    ttype[op]['float']['float'] = 'float'
+
+for op in ['<', '>', '<=', '>=', '==', '!=']:
+    ttype[op]['int']['float'] = 'int'
+    ttype[op]['float']['int'] = 'int'
+    ttype[op]['float']['float'] = 'int'
+
+ttype['+']['string']['string'] = 'string'
+ttype['*']['string']['int'] = 'string'
+
+for op in ['<', '>', '<=', '>=', '==', '!=']:
+    ttype[op]['string']['string'] = 'int'
+
+
+
+class NodeVisitor(object):
+    def visit(self, node):
+        method = 'visit_' + node.__class__.__name__
+        visitor = getattr(self, method, self.generic_visit)
+        return visitor(node)
+
+
+    def generic_visit(self, node):        # Called if no explicit visitor function exists for a node.
+        if isinstance(node, list):
+            for elem in node:
+                self.visit(elem)
+        else:
+            for child in node.children:
+                if isinstance(child, list):
+                    for item in child:
+                        if isinstance(item, AST.Node):
+                            self.visit(item)
+                elif isinstance(child, AST.Node):
+                    self.visit(child)
+
+                    # simpler version of generic_visit, not so general
+                    #def generic_visit(self, node):
+                    #    for child in node.children:
+                    #        self.visit(child)
+
+
+class TypeChecker(NodeVisitor):
+    def visit_BinExpr(self, node):
+    # alternative usage,
+    # requires definition of accept method in class Node
+        type1 = self.visit(node.left)     # type1 = node.left.accept(self) 
+        type2 = self.visit(node.right)    # type2 = node.right.accept(self)
+        op = node.op
+        return ttype[op][type1][type2]
+
+    def visit_RelExpr(self, node):
+        type1 = self.visit(node.left)     # type1 = node.left.accept(self) 
+        type2 = self.visit(node.right)    # type2 = node.right.accept(self)
+        # ... 
+        #
+
+    def visit_Integer(self, node):
+        return 'int'
+
+    def visit_Float(self, node):
+        return 'float'
+
+
+    def visit_Float(self, node):
+        return 'float'
+
