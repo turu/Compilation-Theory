2d9387a08648e9e197bf2d0beed3346e94161b95
turu <piotr.turu.turek@gmail.com>
2014-01-20 11:24:05 +0100

    lab6 init

---
+++ lab6/AST.py
@@ -0,0 +1,177 @@
+class Node(object):
+    def __str__(self):
+        return self.printTree()
+
+
+class Const(Node):
+    def __init__(self, value):
+        self.value = value
+
+
+class Integer(Const):
+    pass
+
+
+class Float(Const):
+    pass
+
+
+class String(Const):
+    pass
+
+
+class Variable(Node):
+    pass
+
+
+class BinExpr(Node):
+    def __init__(self, lhs, op, rhs):
+        self.lhs = lhs
+        self.op = op
+        self.rhs = rhs
+
+
+class ExpressionList(Node):
+    def __init__(self):
+        self.expressionList = []
+
+    def addExpression(self, expr):
+        self.expressionList.append(expr)
+
+
+class GroupedExpression(Node):
+    def __init__(self, interior):
+        self.interior = interior
+
+class FunctionExpression(Node):
+    def __init__(self, retType, name, args, body):
+        self.retType = retType
+        self.name = name
+        self.args = args
+        self.body = body
+
+
+class FunctionExpressionList(Node):
+    def __init__(self):
+        self.fundefs = []
+
+    def addFunction(self, fundef):
+        self.fundefs.append(fundef)
+
+
+class DeclarationList(Node):
+    def __init__(self):
+        self.declarations = []
+
+    def addDeclaration(self, declaration):
+        self.declarations.append(declaration)
+
+
+class Declaration(Node):
+    def __init__(self, type, inits):
+        self.type = type
+        self.inits = inits
+
+
+class InvocationExpression(Node):
+    def __init__(self, name, args):
+        self.name = name
+        self.args = args
+
+   
+class Argument(Node):
+    def __init__(self, type, name):
+        self.type = type
+        self.name = name
+
+
+class ArgumentList(Node):
+    def __init__(self):
+        self.argList = []
+        
+    def addArgument(self, arg):
+        self.argList.append(arg)
+
+
+class InitList(Node):
+    def __init__(self):
+        self.inits = []
+        
+    def addInit(self, init):
+        self.inits.append(init)
+
+
+class Init(Node):
+    def __init__(self, name, expr):
+        self.name = name
+        self.expr = expr
+
+
+class InstructionList(Node):
+    def __init__(self):
+        self.instructions = []
+    
+    def addInstruction(self, instr):
+        self.instructions.append(instr)
+
+
+class PrintInstruction(Node):
+    def __init__(self, expr):
+        self.expr = expr
+
+
+class LabeledInstruction(Node):
+    def __init__(self, id, instr):
+        self.id = id
+        self.instr = instr
+
+
+class AssignmentInstruction(Node):
+    def __init__(self, id, expr):
+        self.id = id
+        self.expr = expr
+
+
+class CompoundInstruction(Node):
+    def __init__(self, declarations, instructions):
+        self.declarations = declarations
+        self.instructions = instructions
+
+
+class ChoiceInstruction(Node):
+    def __init__(self, condition, action, alternateAction=None):
+        self.condition = condition
+        self.action = action
+        self.alternateAction = alternateAction
+
+
+class WhileInstruction(Node):
+    def __init__(self, condition, instruction):
+        self.condition = condition
+        self.instruction = instruction
+
+
+class RepeatInstruction(Node):
+    def __init__(self, instructions, condition):
+        self.instructions = instructions
+        self.condition = condition
+
+
+class ReturnInstruction(Node):
+    def __init__(self, expression):
+        self.expression = expression
+
+
+class BreakInstruction(Node):
+    pass
+
+
+class ContinueInstruction(Node):
+    pass
+
+
+class Program(Node):
+    def __init__(self, declarations, fundefs, instructions):
+        self.declarations = declarations
+        self.fundefs = fundefs
+        self.instructions = instructions
---
+++ lab6/TreePrinter.py
@@ -0,0 +1,129 @@
+import AST
+
+INDENT_TOKEN = "| "
+
+
+def addToClass(cls):
+    def decorator(func):
+        setattr(cls, func.__name__, func)
+        return func
+    return decorator
+
+
+class TreePrinter:
+
+    @addToClass(AST.Node)
+    def printTree(self, indent=0):
+        raise Exception("printTree not defined in class " + self.__class__.__name__)
+
+    @addToClass(AST.Const)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + str(self.value) + "\n"
+
+    @addToClass(AST.ExpressionList)
+    def printTree(self, indent=0):
+        return "".join(map(lambda x: x.printTree(indent + 1), self.expressionList))
+
+    @addToClass(AST.ArgumentList)
+    def printTree(self, indent=0):
+        return "".join(map(lambda x: x.printTree(indent), self.argList))
+
+    @addToClass(AST.Argument)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + "ARG " + self.name + "\n"
+
+    @addToClass(AST.BinExpr)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + self.op + "\n" + self.lhs.printTree(indent + 1) + self.rhs.printTree(indent + 1)
+
+    @addToClass(AST.GroupedExpression)
+    def printTree(self, indent=0):
+        return self.interior.printTree(indent)
+
+    @addToClass(AST.CompoundInstruction)
+    def printTree(self, indent=0):
+        return ("" if self.declarations is None else self.declarations.printTree(indent + 1)) + \
+            self.instructions.printTree(indent + 1)
+
+    @addToClass(AST.LabeledInstruction)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + "LABEL\n" + INDENT_TOKEN * (indent + 1) + str(self.id) + "\n" + \
+               self.instr.printTree(indent + 1)
+
+    @addToClass(AST.InvocationExpression)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + "FUNCALL\n" + INDENT_TOKEN * (indent + 1) + str(self.name) + "\n" + \
+            self.args.printTree(indent+1)
+
+    @addToClass(AST.FunctionExpressionList)
+    def printTree(self, indent=0):
+        return "".join(map(lambda x: x.printTree(indent), self.fundefs))
+
+    @addToClass(AST.FunctionExpression)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + "FUNDEF\n" + INDENT_TOKEN * (indent + 1) + str(self.name) + "\n" + \
+            INDENT_TOKEN * (indent + 1) + "RET " + str(self.retType) + "\n" + self.args.printTree(indent + 1) + \
+            self.body.printTree(indent)
+
+    @addToClass(AST.DeclarationList)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + "DECL\n" + "".join(map(lambda x: x.printTree(indent + 1), self.declarations))
+
+    @addToClass(AST.Declaration)
+    def printTree(self, indent=0):
+        return self.inits.printTree(indent)
+
+    @addToClass(AST.InitList)
+    def printTree(self, indent=0):
+        return "".join(map(lambda x: x.printTree(indent), self.inits))
+
+    @addToClass(AST.Init)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + "=\n" + INDENT_TOKEN * (indent + 1) + str(self.name) + "\n" + \
+            self.expr.printTree(indent + 1)
+
+    @addToClass(AST.InstructionList)
+    def printTree(self, indent=0):
+        return "".join(map(lambda x: x.printTree(indent), self.instructions))
+
+    @addToClass(AST.PrintInstruction)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + "PRINT\n" + self.expr.printTree(indent + 1)
+
+    @addToClass(AST.AssignmentInstruction)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + "=\n" + INDENT_TOKEN * (indent + 1) + str(self.id) + "\n" + \
+            self.expr.printTree(indent + 1)
+
+    @addToClass(AST.ChoiceInstruction)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + "IF\n" + self.condition.printTree(indent + 1) + self.action.printTree(indent + 1) + \
+            ("" if self.alternateAction is None else INDENT_TOKEN * indent + "ELSE\n" +
+                self.alternateAction.printTree(indent + 1))
+
+    @addToClass(AST.WhileInstruction)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + "WHILE\n" + self.condition.printTree(indent + 1) + self.instruction.printTree(indent)
+
+    @addToClass(AST.RepeatInstruction)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + "REPEAT\n" + self.instructions.printTree(indent + 1) + INDENT_TOKEN * indent + \
+            "UNTIL\n" + self.condition.printTree(indent + 1)
+
+    @addToClass(AST.ReturnInstruction)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + "RETURN\n" + self.expression.printTree(indent + 1)
+
+    @addToClass(AST.BreakInstruction)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + "BREAK\n"
+
+    @addToClass(AST.ContinueInstruction)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + "CONTINUE\n"
+
+    @addToClass(AST.Program)
+    def printTree(self, indent=0):
+        return  ("" if self.declarations is None else self.declarations.printTree(indent)) + \
+                ("" if self.fundefs is None else self.fundefs.printTree(indent)) + \
+                self.instructions.printTree(indent+1)
