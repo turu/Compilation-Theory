f1d27766d76d199ee57d20c5664ca1cd48157420
zmuda <zmudamichal92@gmail.com>
2014-01-27 11:59:16 +0100

    lol

--- lab7/Interpreter.py
+++ lab7/Interpreter.py
@@ -10,36 +10,38 @@ class Interpreter(object):
     def visit(self, node):
         pass
 
-    @when(AST.BinOp)
+    @when(AST.BinExpr)
     def visit(self, node):
-        r1 = node.left.accept(self)
-        r2 = node.right.accept(self)
+        r1 = node.lhs.accept(self)
+        r2 = node.rhs.accept(self)
+        eval("a" + node.op + "b", {"a": r1, "b": r2})
         # try sth smarter than:
-        # if(node.op=='+') return r1+r2
         # elsif(node.op=='-') ...
-
-    @when(AST.RelOp)
-    def visit(self, node):
-        r1 = node.left.accept(self)
-        r2 = node.right.accept(self)
-        # ...
+        # if(node.op=='+') return r1+r2
 
     @when(AST.Assignment)
     def visit(self, node):
         pass
-    #
+
+    @when(AST.GroupedExpression)
+    def visit(self, node):
+        pass
+
+        #
     #
 
+    #!
     @when(AST.Const)
     def visit(self, node):
         return node.value
 
+    #!
     # simplistic while loop interpretation
-    @when(AST.WhileInstr)
+    @when(AST.WhileInstruction)
     def visit(self, node):
         r = None
-        while node.cond.accept(self):
-            r = node.body.accept(self)
+        while node.condition.accept(self):
+            r = node.instruction.accept(self)
         return r
 
 
